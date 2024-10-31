baa7d2b99dc91ec17fd1ed9873104c74d43973b3
Uriel Pérez Sánchez <urielpes.ups@gmail.com>
2024-10-30 21:42:44 -0600

    ci

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,5 +7,5 @@ import { provideHttpClient } from '@angular/common/http';
 
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync(), provideAnimationsAsync(), provideAnimationsAsync(), provideHttpClient(), provideAnimationsAsync()]
+  providers: [provideRouter(routes), provideAnimationsAsync(), provideAnimationsAsync(), provideAnimationsAsync(), provideHttpClient(), provideAnimationsAsync(), provideAnimationsAsync()]
 };
